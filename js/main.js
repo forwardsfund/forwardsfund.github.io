322b6ea9013190dca1c7c72d4a015db52bd817d5
Jeffrey Laederach <jefflae@outlook.com>
2025-05-15 22:08:59 -0400

    Initial Website Commit

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// Close the navbar when a nav-item or dropdown-item is clicked
+document.querySelectorAll('.navbar-nav .nav-link, .dropdown-item').forEach(item => { 
+    item.addEventListener('click', (event) => {
+        // Check if the clicked item is not part of a dropdown toggle
+        if (!event.target.closest('.dropdown-toggle')) {
+            if (document.querySelector('.navbar-collapse').classList.contains('show')) { 
+                document.querySelector('.navbar-toggler').click(); 
+            }
+        }
+    }); 
+});
+
+function openOverlay(id) {
+    document.getElementById(id).style.display = "flex";
+}
+
+function closeOverlay(id) {
+    document.getElementById(id).style.display = "none";
+}
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            const boxes = document.querySelectorAll('.slide-box');
+            boxes.forEach((box, index) => {
+                setTimeout(() => {
+                    box.classList.add('animate');
+                }, index * 200); // 200ms delay between each box
+            });
+        }
+    });
+}, {
+    threshold: 0.2
+});
+
+observer.observe(document.querySelector('#about'));
